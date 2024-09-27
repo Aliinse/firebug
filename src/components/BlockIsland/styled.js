0a7ec2ec7eb9d1d921354114cd191b61ad1e97b2
Danil <gond@crtweb.ru>
2024-09-28 00:42:05 +0300

    -fix styles

--- src/components/BlockIsland/styled.js
+++ src/components/BlockIsland/styled.js
@@ -5,11 +5,3 @@ export const SIslandBlock = styled.div`
   box-shadow: rgba(0, 0, 0, 0.05) -3px -3px 10px, rgba(79, 79, 79, 0.05) 5px 5px 10px;
 	padding: 30px;
 `
-export const QAIslandBlock = styled.div`
-  border-radius: 12px;
-  box-shadow: rgba(0, 0, 0, 0.05) -3px -3px 10px, rgba(79, 79, 79, 0.05) 5px 5px 10px;
-  padding: 30px;
-  background-color: #fff;
-  flex: ${(props) => props.flex || '1'};
-  box-sizing: border-box;
-`;
--- src/components/ManualTestingPage/styled.js
+++ src/components/ManualTestingPage/styled.js
@@ -1,27 +1,23 @@
 import styled from "styled-components";
-import {SIslandBlock} from "../BlockIsland/styled"
+import { SIslandBlock } from "../BlockIsland/styled";
 
 export const QAIsland = styled(SIslandBlock)`
- display: flex;
-  justify-content: center;
-  align-items: center;
+  display: flex;
+  flex-direction: column;
   margin: 0 auto;
   max-width: 1654px;
   width: 100%;
-`
+`;
 export const QAContent = styled.div`
-padding: 150px 20px;
-font-family: sans-serif;
-text-align: center;
-`
+  padding: 150px 20px;
+  font-family: sans-serif;
+  text-align: center;
+`;
 
 export const QAStyleContainer = styled.div`
-  display: flex;
-  gap: 20px; /* Расстояние между блоками */
   width: 100%;
-  flex-wrap: wrap;
+`;
 
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
+export const SParagraph = styled.p`
+  text-align: justify;
 `;
